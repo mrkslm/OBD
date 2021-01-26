d1abdfa81b7361798ec877b9a33bbd364b183c57
Marek <65215231+mrkslm@users.noreply.github.com>
2021-01-26 11:34:31 +0200

    2

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #! python3
 import obd
 
-connection = obd.OBD()
-cmd = obd.commands.SPEED
-
-response = connection.query(cmd)
-print("the speed is" + response.value)
-print("the speed in kph is" + response.value.to("kph"))
+ports = obd.scan_serial()  # return list of valid USB or RF ports
+# print(ports)
+connection = obd.OBD(ports[1])  # connect to the second port in the list
 
 
-rpm = connection.query(obd.commands.RMP)
-print("RPM is " + rpm)
+def main():
+    print('Press Ctrl-C to quit.')
+    try:
+        while True:
+            r = connection.query(obd.commands.RPM)  # returns the response from the car
+            r2 = connection.query(obd.commands.SPEED)  # returns the response from the car
+            print(r)
+            print(r2)
+    except KeyboardInterrupt:
+        print('\nDone')
 
+
+if __name__ == '__main__':
+    main()
